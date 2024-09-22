d896a5fcd77361dfe7914d946e36ea695672a79e
Arturo <art2dit2@hotmail.com>
2024-09-22 09:55:32 +0200

    Delete apiKey

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,5 +1,5 @@
 export const getGifts = async (category) => {
-    const apiKey = `34XpBLnjongG29VM2kdAUsGZx1R7jt9c&q`;
+    const apiKey ='fake';
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
