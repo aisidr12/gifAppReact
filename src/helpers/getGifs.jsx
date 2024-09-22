2e76da7bfb697023e480b57c664371094c4a50f5
Arturo <art2dit2@hotmail.com>
2024-09-22 09:54:47 +0200

    Redo apikey

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,6 +1,6 @@
 export const getGifts = async (category) => {
-    
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=34XpBLnjongG29VM2kdAUsGZx1R7jt9c&q=${ category }&limit=10`
+    const apiKey = `34XpBLnjongG29VM2kdAUsGZx1R7jt9c&q`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${ category }&limit=10`
     const resp = await fetch( url );
     const { data } = await resp.json();
 
